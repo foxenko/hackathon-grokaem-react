4292e1d6c3275c90f4b3128accee8fb4b57e5466
foxenko <ilyabro65410@gmail.com>
2024-05-05 19:34:18 +0500

    add images

--- backend/data/utils/parse.js
+++ backend/data/utils/parse.js
@@ -1,15 +1,15 @@
 const fs = require('fs').promises
 const readData = async () => {
     try{
-        const result = await fs.readFile('', "utf8")
+        const result = await fs.readFile('./data/cards.json', "utf8")
         const data = JSON.parse(result)
         return data
     }catch(err){
         console.log(err)
     }
 }
-const writeData = async (url, data) => {
+const writeData = async (data) => {
     const jsonData = JSON.stringify(data)
-    await fs.writeFile(url, jsonData)
+    await fs.writeFile('./data/cards.json', jsonData)
 }
 module.exports = {readData, writeData}
--- backend/utils/getDataByName.js
+++ backend/utils/getDataByName.js
@@ -1,1 +1,10 @@
-const { readData, writeData } = require('')
+const { readData, writeData } = require('./data/cards.json')
+const getDataByName = async (req, res) => {
+    const searchName = req.body
+    const data = await readData()
+    const obj = data[0]
+    for (let key in obj){
+        console.log(obj.key)
+    }
+}
+module.exports = { getDataByName }
